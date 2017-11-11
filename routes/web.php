080546d467fc5b343ebb92d9f9f4d02735273088
ankita <ankita.parmar0504@gmail.com>
2017-11-11 17:06:05 +0530

    Employee & security question module

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,15 @@ Route::get('/', function () {
 
 $this->get('index', 'HomeController@index');
 Route::resource('company', 'CompanyController');
+Route::resource('security_question', 'SecurityQuestionController');
+Route::resource('employee', 'EmployeeController');
 $this->get('get_company', 'CompanyController@get_company');
+$this->get('get_security_question','SecurityQuestionController@get_security_question');
+$this->get('get_employee', 'EmployeeController@get_company_employee');
 Auth::routes();
     
     Route::group([ 'middleware' => 'admin' ] , function () {
+        Route::match(['get','post'],'user/list','UserController@userListing')->name('user.list');
         Route::resource('user','UserController');
     });
     
