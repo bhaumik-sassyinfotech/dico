d1a697dda1fce1f4e184d9e1a4ac51ce6f3a3ed2
Bhaumik Mehta <bhaumik.sassyinfotech@gmail.com>
2017-11-25 18:59:37 +0530

    Bug fix.

--- app/MeetingUser.php
+++ app/MeetingUser.php
@@ -3,9 +3,10 @@
 namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+class MeetingUser extends Model
 {
     //
     use SoftDeletes;
     protected $dates = ['deleted_at'];
+    protected $fillable = ['meeting_id','user_id','is_admin','created_at','updated_at'];
 }
