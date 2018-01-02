ab03ff8be34d2e1eb5d108b9832b77dc12042376
Bhaumik Mehta <bhaumik.sassyinfotech@gmail.com>
2018-01-02 18:23:28 +0530

    solve conflicts

--- resources/views/companyadmin/groups/create.blade.php
+++ resources/views/companyadmin/groups/create.blade.php
@@ -1,87 +1,104 @@
 @extends('template.default')
 @section('content')
 
-    
     <div id="page-content">
         <div id='wrap'>
             <div id="page-heading">
-                <div class="row">
-                    <div class="col-md-6">
-                        <h1>Create Group</h1>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="pull-right nopadding"><p style="float:right;"><a
-                                        href="{{ url('/home') }}">Dashboard</a> > <a href="{{ route('group.index') }}">Group</a>
-                                > Create Group</p></div>
-                    </div>
-                </div>
+                <ol class="breadcrumb">
+                    <li><a href="{{ url('/home') }}">Dashboard</a></li>
+                    <li><a href="{{ route('group.index') }}">Group</a></li>
+                    <li class="active">Create Group</li>
+                </ol>
+                <h1>Create Group</h1>
+
             </div>
+            {{ Form::open([ 'name' => 'createUserGroup','route' => 'group.store' , 'id' => 'createUserGroup'] ) }}
             <div class="container">
                 <div class="panel panel-default">
-                    <div class="panel panel-body">
+                    {{--<div class="panel-heading">--}}
+                        {{--<h4>Create Group</h4>--}}
+                    {{--</div>--}}
+                    <div class="panel-body">
                         @if(session()->has('success'))
-                        <div class="alert alert-success">
-                            {{ session()->get('success') }}
-                        </div>
+                            <div class="alert alert-dismissable alert-success">
+                                {{ session()->get('success') }}
+                            </div>
                         @endif
                         @if(session()->has('err_msg'))
-                        <div class="alert alert-danger">
-                            {{ session()->get('err_msg') }}
-                        </div>
+                            <div class="alert alert-dismissable alert-danger">
+                                {{ session()->get('err_msg') }}
+                            </div>
                         @endif
                         @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         @endif
-                        <div class="row">
-                            {{ Form::open([ 'name' => 'createUserGroup','route' => 'group.store' , 'id' => 'createUserGroup'] ) }}
-                            <div class="form-group">
+                        <div class="form-group">
+                            <div class="row">
                                 <div class="col-md-6">
                                     <label for="group_name">Group name:*</label>
-                                    <input type="text" name="group_name" id="group_name" class="form-control required"/>
+                                    <input type="text" name="group_name" id="group_name" class="form-control required" value=" {{ old('group_name') }}"/>
                                 </div>
                                 <div class="col-md-6">
-                                    <label for="group_description"> Group Description:</label>
+                                    <label for="group_description"> Group Description:* </label>
                                     <textarea name="group_description" id="group_description"
-                                              class="form-control required"></textarea>
+                                              class="form-control">{{ old('group_description') }}</textarea>
                                 </div>
                             </div>
-                            <br>
-                            <div class="form-group">
-                                <div class="col-md-6">
+                        </div>
+
+                        <div class="form-group ">
+                            <div class="row">
+                                <div class="col-md-3">
                                     <label for="company_listing">Company:* </label>
                                     <select name="company_listing" class="form-control required" id="company_listing">
                                         <option value="">Select Company:*</option>
-                                        @foreach($companies as $company)
-                                            <option value="{{ $company->id }}">{{ $company->company_name }}</option>
-                                        @endforeach
+                                        @if(!is_null(old('company_listing')))
+                                            <?php $company_id = old('company_listing'); ?>
+                                            @foreach($companies as $company)
+                                                <option {{ $company_id == $company->id ? 'selected' : '' }} value="{{ $company->id }}">{{ $company->company_name }}</option>
+                                            @endforeach
+                                        @else
+                                            @foreach($companies as $company)
+                                                <option value="{{ $company->id }}">{{ $company->company_name }}</option>
+                                            @endforeach
+                                        @endif
+                                    </select>
+                                </div>
+                                <div class="col-md-3">
+                                    <label for="group_owner">Group owner:* </label>
+                                    <select name="group_owner" id="group_owner" class="form-control required">
+                                        <option value="">Select user</option>
                                     </select>
                                 </div>
                                 <div class="col-md-6">
-                                    <label for="users_listing">Select Users:* </label>
+                                    <label for="users_listing">Users:* </label>
                                     <select name="users_listing[]" id="users_listing" class="form-control required"
                                             multiple="multiple">
                                         <option value="">Select company first</option>
                                     </select>
                                 </div>
                             </div>
-                            <br>
-                            <div class="form-group">
-                                <div class="col-md-12">
-                                    {{ Form::submit('Save',['class' => 'btn btn-success']) }}
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="btn-toolbar">
+                                    <a class="btn btn-default" href="{{ route('group.index') }}">Back</a>
+                                    {{ Form::submit('Submit',['class' => 'btn btn-primary']) }}
                                 </div>
                             </div>
-                            {{ Form::close() }}
                         </div>
                     </div>
                 </div>
-
             </div>
+            {{ Form::close() }}
         </div>
     </div>
 @stop
